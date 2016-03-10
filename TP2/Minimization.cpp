ca921018220cb8dcecd959e5e7583f3077e6db0e
hmallow <adrien.bruchet.gt@gmail.com>
2016-03-10 11:10:50 +0100

    fichiers minimisation inutiles donc supprimés

--- TP2/Minimization.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Minimization.cpp
-//  MonteCarlo
-//
-//  Created by Adrien Bruchet on 07/03/2016.
-//  Copyright (c) 2016 Adrien Bruchet. All rights reserved.
-//
-
-#include "Minimization.h"
