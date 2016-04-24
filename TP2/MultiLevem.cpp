875d131544c0b6f052cd543f9fe06a94f9873884
hmallow <adrien.bruchet.gt@gmail.com>
2016-04-24 10:47:24 +0100

    creation classe multilevel

---
+++ TP2/MultiLevem.cpp
@@ -0,0 +1,9 @@
+//
+//  MultiLevem.cpp
+//  MonteCarlo
+//
+//  Created by Adrien Bruchet on 24/04/2016.
+//  Copyright (c) 2016 Adrien Bruchet. All rights reserved.
+//
+
+#include "MultiLevem.h"
---
+++ TP2/MultiLevem.h
@@ -0,0 +1,14 @@
+//
+//  MultiLevem.h
+//  MonteCarlo
+//
+//  Created by Adrien Bruchet on 24/04/2016.
+//  Copyright (c) 2016 Adrien Bruchet. All rights reserved.
+//
+
+#ifndef __MonteCarlo__MultiLevem__
+#define __MonteCarlo__MultiLevem__
+
+#include <stdio.h>
+
+#endif /* defined(__MonteCarlo__MultiLevem__) */
